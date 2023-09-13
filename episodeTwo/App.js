a3839ee94bd802855088b988c739b6acfc14bf48
Shashwath B C <shashwath.bc@taazaa.com>
2023-09-13 21:33:12 +0530

    app changes

--- episodeTwo/App.js
+++ episodeTwo/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const index = () => {
-  return (
-    <div  id='root'>
-      index
-      </div>
-  )
-}
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "by Akshay Saini"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 
-export default index
+//JSX
+
+console.log(parent); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
